22acba5d6901746111fcff9295a4cdddd6f52c87
SonjaReimannThanisch <137377644+Sonja40@users.noreply.github.com>
2024-06-13 22:14:07 +0200

    noch ein X Icon hinzugefügt und farben angepasst der Icons und auch die stellungen der Icons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let images = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg', 'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg', 'img/17.jpg', 'img/18.jpg', 'img/19.jpg', 'img/20.jpg', 'img/21.jpg', 'img22.jpg', 'img/23.jpg', 'img/24.jpg', ];
+let images = ['img/1.jpg', 'img/2.jpg', 'img/3.jpg', 'img/4.jpg', 'img/5.jpg', 'img/6.jpg', 'img/7.jpg', 'img/8.jpg', 'img/9.jpg', 'img/10.jpg', 'img/11.jpg', 'img/12.jpg', 'img/13.jpg', 'img/14.jpg', 'img/15.jpg', 'img/16.jpg', 'img/17.jpg', 'img/18.jpg', 'img/19.jpg', 'img/20.jpg', 'img/21.jpg', 'img/22.jpg', 'img/23.jpg', 'img/24.jpg', ];
 
 function render() {
     let content = document.getElementById('pictureOverview');
 
     content.innerHTML = '';
-    for (let i = 0; i >images.length; i++) {
+    for (let i = 0; i < images.length; i++) {
         let image = images[i];
 
         content.innerHTML += `
@@ -22,15 +22,19 @@ function currentImage(i) {
     document.body.style.overflow = 'hidden';
     content.innerHTML = `
     <div class="open-image">
-        <img onclick="previousImage(${i})" class="icons" src="./img/zuruck.png">
-        <img onclick="closeImage()" class="current-image" id="currentImage()" src="${image}">
-        <img onclick="nextImage(${i})" class="icons" src="./img/wiederholen.png">
+        <img onclick="previousImage(${i})" class="icons" src="./img/linker-pfeil (1).png">
+        <img onclick="closeImage()" class="current-image" src="${image}">
+        <img onclick="closeImage()" class="close-icon" src="./img/norwegen.png">
+        <img onclick="closeImage()" class="close" src="./img/chi.png">
+        <img onclick="nextImage(${i})" class="icons-forward" src="./img/rechter-pfeil.png">
     </div>
     `;
+    console.log('Image opened:', image);
+    console.log('Close icon element:', document.querySelector('.close-icon'));
 }
 
 function previousImage(i) {
-    if (i > o) {
+    if (i > 0) {
         i--;
         currentImage(i);
     } else {
